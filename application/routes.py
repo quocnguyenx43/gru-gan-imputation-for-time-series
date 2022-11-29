d91bfaf3d5aaa89931a7f2d9bcb50136449ad9f8
quocnguyen <43xquocnguyen@gmail.com>
2022-11-29 20:03:20 +0700

    xoa change.html

--- application/routes.py
+++ application/routes.py
@@ -8,22 +8,33 @@ from application import vis
 def index():
     return render_template('index.html', title='INDEX')
 
-@app.route('/dashboard')
+
+@app.route('/dashboard', methods=['GET', 'POST'])
 def dashboard():
-    category = request.args.get('category', default='MO')
-    target = request.args.get('target', default='Temperature')
+
+    form = UserInputForm()
+
+    if request.method == "POST":
+        category = form.category.data
+        target = form.feature.data
+    else:
+        category = 'MO'
+        target = 'Temperature'
+
+    # category = request.args.get('category', default='MO')
+    # target = request.args.get('target', default='Temperature')
 
     script0, div0 = vis.distribution_plot(target)
     script1, div1 = vis.boxplot_chart(category, target)
     script2, div2 = vis.avg_per_category_barplot(category, target)
     script3, div3 = vis.avg_quarter_year_barplot(target)
     script4, div4 = vis.line_circle_plot_top_10(target, 10)
-
     script = [script0] + [script1] + [script2] + [script3] + [script4]
     div = [div0] + [div1] + [div2] + [div3] + [div4]
 
     return render_template(
-        'dashboard.html', 
+        'dashboard.html',
+        form=form,
         script=script,
         div=div,
         cdn_css=vis.cdn_css,
@@ -31,12 +42,7 @@ def dashboard():
         title='DASHBOARD'
     )
 
-@app.route('/change', methods=['GET', 'POST'])
-def change():
-    form = UserInputForm()
-    if request.method == "POST":
-        category_feature = form.category.data
-        target_feature = form.feature.data
-        return redirect(url_for('dashboard', category=category_feature, target=target_feature))
-    else:
-        return render_template('change.html', title='CHANGE', form=form)
+
+@app.route('/gan')
+def gan():
+    return render_template('gan.html', title='INDEX')
--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from application import app
 
 if __name__ == '__main__':
-    #app.run(debug=True)
-    app.run(debug=False, host='0.0.0.0')
+    app.run(debug=True)
+    #app.run(debug=False, host='0.0.0.0')
